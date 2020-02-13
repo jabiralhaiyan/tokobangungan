908deaeb9d4b0c6f25d4559269100f8e24a4d166
stonear <27681405+stonear@users.noreply.github.com>
2020-02-13 22:16:01 +0700

    add Indonesian Language

--- app/language/indonesian/auth_lang.php
+++ app/language/indonesian/auth_lang.php
@@ -2,10 +2,10 @@
 
 /*
  * Module: Auth
- * Language: English
+ * Language: Indonesian
  * 
  * Last edited:
- * 30th April 2015
+ * 08th February 2020
  *
  * Package:
  * Simple POS - Point of sale made easy
@@ -16,79 +16,79 @@
  * Thank you 
  */
 
-$lang['sign_in']                                    = "Sign in";
-$lang['notify_user_by_email']                       = "Notify User by Email";
-$lang['group']                                      = "Group";
-$lang['edit_user']                                  = "Edit User";
-$lang['delete_user']                                = "Delete User";
-$lang['user_added']                                 = "User successfully added";
-$lang['user_updated']                               = "User successfully updated";
-$lang['users_deleted']                              = "Users successfully deleted";
-$lang['alert_x_user']                               = "You are going to remove this user permanently. Press OK to proceed and Cancel to Go Back";
+$lang['sign_in']                                    = "Masuk";
+$lang['notify_user_by_email']                       = "Beri tahu Pengguna melalui Email";
+$lang['group']                                      = "Grup";
+$lang['edit_user']                                  = "Ubah pengguna";
+$lang['delete_user']                                = "Hapus pengguna";
+$lang['user_added']                                 = "Berhasil menambah pengguna";
+$lang['user_updated']                               = "Berhasil memperbarui pengguna";
+$lang['users_deleted']                              = "Berhasil menghapus pengguna";
+$lang['alert_x_user']                               = "Anda akan menghapus pengguna ini secara permanen. Tekan OK untuk melanjutkan dan Batal untuk kembali.";
 $lang['login_email']                                = "Login Email";
-$lang['edit_profile']                               = "Edit Profile";
+$lang['edit_profile']                               = "Perbarui profil";
 $lang['website']                                    = "Website";
-$lang['if_you_need_to_rest_password_for_user']      = "If you need to reset the password for this user.";
-$lang['user_options']                               = "User options";
-$lang['old_password']                               = "Old Password";
-$lang['new_password']                               = "New Password";
-$lang['change_avatar']                              = "Change Avatar";
-$lang['update_avatar']                              = "Update Avatar";
+$lang['if_you_need_to_rest_password_for_user']      = "Jika anda ingin mereset kata sandi untuk pengguna ini.";
+$lang['user_options']                               = "Opsi pengguna";
+$lang['old_password']                               = "Kata sandi lama";
+$lang['new_password']                               = "Kata sandi baru";
+$lang['change_avatar']                              = "Ganti Avatar";
+$lang['update_avatar']                              = "Perbarui Avatar";
 $lang['avatar']                                     = "Avatar";
-$lang['avatar_deleted']                             = "Avatar successfully updated";
-$lang['captcha_wrong']                              = "Captcha is wrong or expired. Please try again";
+$lang['avatar_deleted']                             = "Avatar berhasil diperbarui";
+$lang['captcha_wrong']                              = "Captcha salah atau kadaluarsa. Silahkan coba kembali";
 $lang['captcha']                                    = "Captcha";
-$lang['site_is_offline_plz_try_later']              = "Site is offline. Please visit us again in few days.";
-$lang['type_captcha']                               = "Type Captcha";
-$lang['we_are_sorry_as_this_sction_is_still_under_development'] = "We are sorry as this section is still under development and we are try to make it asap. Your patient is highly appreciated.";
-$lang['confirm']                                    = "Confirmation";
-$lang['error_csrf']                                 = "Cross-site request forgery detected or the csrf token is expired. Please try again.";
-$lang['avatar_updated']                             = "Avatar successfully updated";
-$lang['registration_is_disabled']                   = "Account Registration is closed.";
-$lang['login_to_your_account']                      = "Please login to your account.";
-$lang['pw']                                         = "Password";
-$lang['remember_me']                                = "Remember me";
-$lang['forgot_your_password']                       = "Forgot your password?";
-$lang['dont_worry']                                 = "Don't worry!";
-$lang['click_here']                                 = "click here";
-$lang['to_rest']                                    = "to reset";
-$lang['forgot_password']                            = "Forgot Password?";
-$lang['login_successful']                           = "You are successfully logged in.";
-$lang['back']                                       = "Back";
-$lang['dont_have_account']                          = "Don't have account?";
-$lang['no_worry']                                   = "No worries!";
-$lang['to_register']                                = "to register";
-$lang['register_account_heading']                   = "Please fill in the form below to register account";
-$lang['register_now']                               = "Register Now";
-$lang['no_user_selected']                           = "No user selected. Please select at least one user.";
-$lang['delete_users']                               = "Delete Users";
-$lang['delete_avatar']                              = "Delete Avatar";
-$lang['deactivate_heading']                         = "Are you sure to deactivate the user?";
-$lang['deactivate']                                 = "Deactivate";
-$lang['pasword_hint']                               = "At least 1 capital, 1 lowercase, 1 number and more than 8 characters long";
-$lang['pw_not_same']                                = "The password and confirm password are not the same";
-$lang['reset_password']                             = "Reset Password";
-$lang['reset_password_link_alt']                    = "You can paste this code below in your url if the above link not working";
-$lang['email_forgotten_password_subject']           = "Reset Password Details";
-$lang['reset_password_email']                       = "Reset Password %s";
-$lang['back_to_login']                              = "Back to login";
-$lang['forgot_password_unsuccessful']               = "Reset password failed";
-$lang['forgot_password_successful']                 = "Password successfully reset, please use new password to login";
-$lang['password_change_unsuccessful']               = "Password change failed";
-$lang['password_change_successful']                 = "Password successfully changed";
-$lang['forgot_password_email_not_found']            = "The email address enter does not belong to any account.";
-$lang['login_unsuccessful']                         = "Login Failed, Please try again";
-$lang['email_forgot_password_link']                 = "Reset Password Link";
-$lang['reset_password_heading']                     = "Reset Password";
-$lang['reset_password_new_password_label']          = "New Password";
-$lang['reset_password_new_password_confirm_label']  = "Confirm New Password";
-$lang['register']                                   = "Register";
-$lang['reset_password_submit_btn']                  = "Reset Password";
-$lang['error_csrf']                                 = 'This form post did not pass our security checks.';
-$lang['account_creation_successful']                = "Account successfully created";
-$lang['old_password_wrong']                         = "Please type correct old password";
-$lang['sending_email_failed']                       = "Unable to send email, If you are admin then please check your settings otherwise contact admin.";
-$lang['deactivate_successful']                      = "User successfully deactivated";
-$lang['activate_successful']                        = "User successfully activated";
-$lang['login_timeout']                              = "You have 3 failed login attempts. Please try after 10 minutes";
-$lang['forgot_password_heading']                    = "Enter your e-mail address below to reset your password.";
+$lang['site_is_offline_plz_try_later']              = "Site sedang offline. Silahkan kunjungi lagi dalam beberapa hari.";
+$lang['type_captcha']                               = "Tulis Captcha";
+$lang['we_are_sorry_as_this_sction_is_still_under_development'] = "Kami mohon maaf karena bagian ini masih dalam pengembangan dan kami sedang mencoba membuatnya sesegera mungkin. Kesabaran anda sangat dihargai.";
+$lang['confirm']                                    = "Konfirmasi";
+$lang['error_csrf']                                 = "Cross-site request forgery terdeteksi atau csrf token kadaluarsa. Silahkan coba lagi.";
+$lang['avatar_updated']                             = "Avatar berhasil diperbarui";
+$lang['registration_is_disabled']                   = "Pendaftaran akun ditutup.";
+$lang['login_to_your_account']                      = "Silahkan masukkan akun anda.";
+$lang['pw']                                         = "Kata sandi";
+$lang['remember_me']                                = "Ingat Saya";
+$lang['forgot_your_password']                       = "Lupa kata sandi anda?";
+$lang['dont_worry']                                 = "Jangan khawatir!";
+$lang['click_here']                                 = "klik disini";
+$lang['to_rest']                                    = "untuk reset";
+$lang['forgot_password']                            = "Lupa kata sandi?";
+$lang['login_successful']                           = "Anda berhasil masuk.";
+$lang['back']                                       = "Kembali";
+$lang['dont_have_account']                          = "Belum punya akun?";
+$lang['no_worry']                                   = "Jangan khawatir!";
+$lang['to_register']                                = "untuk mendaftar";
+$lang['register_account_heading']                   = "Silakan isi formulir di bawah ini untuk mendaftarkan akun";
+$lang['register_now']                               = "Daftar Sekarang";
+$lang['no_user_selected']                           = "Tidak ada pengguna yang dipilih. Silakan pilih setidaknya satu pengguna.";
+$lang['delete_users']                               = "Hapus Pengguna";
+$lang['delete_avatar']                              = "Hapus Avatar";
+$lang['deactivate_heading']                         = "Apakah anda yakin akan menonaktifkan pengguna?";
+$lang['deactivate']                                 = "Menonaktifkan";
+$lang['pasword_hint']                               = "Setidaknya 1 huruf kapital, 1 huruf kecil, 1 angka, dan lebih dari 8 karakter";
+$lang['pw_not_same']                                = "Kata sandi dan kata sandi konfirmasi tidak sama";
+$lang['reset_password']                             = "Setel ulang Kata sandi";
+$lang['reset_password_link_alt']                    = "Anda dapat menempelkan kode di bawah ini di url anda jika tautan di atas tidak berfungsi";
+$lang['email_forgotten_password_subject']           = "Setel ulang Detail Kata sandi";
+$lang['reset_password_email']                       = "Setel ulang Kata sandi %s";
+$lang['back_to_login']                              = "Kembali untuk masuk";
+$lang['forgot_password_unsuccessful']               = "Gagal mengatur ulang kata sandi";
+$lang['forgot_password_successful']                 = "Kata sandi berhasil diatur ulang, harap gunakan kata sandi baru untuk masuk";
+$lang['password_change_unsuccessful']               = "Pembaruan kata sandi gagal";
+$lang['password_change_successful']                 = "Kata sandi berhasil diperbarui";
+$lang['forgot_password_email_not_found']            = "Masukkan alamat email bukan milik akun mana pun.";
+$lang['login_unsuccessful']                         = "Gagal masuk, Silakan coba lagi";
+$lang['email_forgot_password_link']                 = "Tautan Setel ulang Kata sandi";
+$lang['reset_password_heading']                     = "Setel ulang Kata sandi";
+$lang['reset_password_new_password_label']          = "Kata sandi Baru";
+$lang['reset_password_new_password_confirm_label']  = "Konfirmasi Kata sandi Baru";
+$lang['register']                                   = "Daftar";
+$lang['reset_password_submit_btn']                  = "Setel ulang Kata sandi";
+$lang['error_csrf']                                 = 'Formulir POST ini tidak lulus pemeriksaan keamanan kami.';
+$lang['account_creation_successful']                = "Akun berhasil dibuat";
+$lang['old_password_wrong']                         = "Harap ketikkan kata sandi lama yang benar";
+$lang['sending_email_failed']                       = "Tidak dapat mengirim email, Jika Anda admin, maka silakan periksa pengaturan Anda. Jika tidak, hubungi admin.";
+$lang['deactivate_successful']                      = "Pengguna berhasil dinonaktifkan";
+$lang['activate_successful']                        = "Pengguna berhasil diaktifkan";
+$lang['login_timeout']                              = "Anda telah gagal masuk 3 kali. Silakan coba lagi setelah 10 menit";
+$lang['forgot_password_heading']                    = "Masukkan alamat email Anda di bawah ini untuk mengatur ulang kata sandi Anda.";
